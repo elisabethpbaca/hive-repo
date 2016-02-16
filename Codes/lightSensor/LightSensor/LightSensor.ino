df7dad28ec9d488e7db5fc060f9d753367e62aea
rhane <emmarhanenapier@gmail.com>
2016-02-16 15:20:14 -0500

    hip dday
    edited taLking file

--- Codes/lightSensor/LightSensor/LightSensor.ino
+++ Codes/lightSensor/LightSensor/LightSensor.ino
@@ -1,19 +1,19 @@
 int sensorPin = A1; // select the input pin for the potentiometer
-int ledPin = 13; // select the pin for the LED
+//int ledPin = 13; // select the pin for the LED
 int sensorValue = 0; // variable to store the value coming from the sensor
 
 // Min 0
 // Max 1024
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+//  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
 void loop() {
   sensorValue = analogRead(sensorPin);
-  digitalWrite(ledPin, HIGH);
-  delay(sensorValue);
-  digitalWrite(ledPin, LOW);
-  delay(sensorValue);
+  //digitalWrite(ledPin, HIGH);
+  delay(100);
+//  digitalWrite(ledPin, LOW);
+  delay(100);
   Serial.println(sensorValue, DEC);
 }
